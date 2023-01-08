e6f96667caf978026f58860581ead98c1d8c4bab
Raphytator <r.marcon26@gmail.com>
2023-01-08 02:49:55 +0100

    Add more introduction story

--- scene_introduction.lua
+++ scene_introduction.lua
@@ -16,17 +16,23 @@ function intro.init()
     sprites.intro[2] = creaSprite(introImg[2], 0, 0)
     sprites.intro[3] = creaSprite(introImg[1], 0, 0)
     sprites.intro[4] = creaSprite(introImg[3], 0, 0)
+    sprites.intro[5] = creaSprite(introImg[1], 0, 0)
+    sprites.intro[6] = creaSprite(introImg[2], 0, 0)
     
     local txt1 = "Honney, we need to buy a new car !"
     local txt2 = "How much do we need ?"
-    local txt3 = "Around $250.000"
-    local txt4 = "Hum..... ok honney"
+    local txt3 = "Around $250.000."
+    local txt4 = "Hum..... ok honney, i will sell some veggies, but that will take time."
+    local txt5 = "I need it quickly !!"
+    local txt6 = "I should try something different to increase the value of veggies."
         
     txt.intro = {}
     txt.intro[1] = creaTxt(txt1, _fonts.titre, 500, 80, {0,0,0,1}, 750, "left")
     txt.intro[2] = creaTxt(txt2, _fonts.titre, 50, 80, {0,0,0,1}, 750, "left")
     txt.intro[3] = creaTxt(txt3, _fonts.titre, 500, 80, {0,0,0,1}, 750, "left")
     txt.intro[4] = creaTxt(txt4, _fonts.titre, 50, 80, {0,0,0,1}, 750, "left")
+    txt.intro[5] = creaTxt(txt5, _fonts.titre, 500, 80, {0,0,0,1}, 750, "left")
+    txt.intro[6] = creaTxt(txt6, _fonts.titre, 50, 80, {0,0,0,1}, 750, "left")
 
     txt.continue = creaTxt("Click to continue", _fonts.titre, 0, 620, {0,0,0,1}, _ecran.w, "center")
 end 
@@ -36,7 +42,7 @@ function intro.load()
 end 
 
 function intro.update(dt)
-    local etatFinal = 4
+    local etatFinal = 6
     if love.mouse.isDown(1) and not _clic then 
         _clic = true
         if etatIntro < etatFinal then 
